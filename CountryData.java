86ef0c12fa92fd46f8398df483482aac76e804b8
echatfield61 <echatfield61@gmail.com>
2024-05-22 13:28:10 -0400

    yay

---
+++ CountryData.java
@@ -0,0 +1,12 @@
+public class CountryData 
+{
+    private String name;
+    private int year;
+    private int population;
+    private int[] incomeDeciles;
+    public CountryData(Data incomeData, String country, String year)
+    {
+        
+    }
+    
+}
