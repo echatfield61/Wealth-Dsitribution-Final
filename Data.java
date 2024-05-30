a067a1e2b5034671934bdc70886221f6e0cbaa45
echatfield61 <echatfield61@gmail.com>
2024-05-30 13:30:17 -0400

    yay

--- Data.java
+++ Data.java
@@ -23,18 +23,39 @@ public class Data {
       
       /* populate unformattedData array with data from the table */
       /* use String method replaceAll to get rid double quotes */
-      
+       
       
       /* parse through the first row of unformattedData to num of columns */
-      
+      int yay = 0;
+      for (int i = 0; i < unformattedData.get(0).length();i++)
+      {
+        if(unformattedData.get(0).substring(i,i+1).equals(","))
+        {
+          yay++;
+        }
+      }
       
       
       /* initialize the formattedData array using rows and columns */
       
-      
+      formattedData = new String[c][yay];
       
       /* populate formattedData array with data from unformattedData */
-      
+      int base = 0; 
+      int t = 0;
+      for (int x=0; x<unformattedData.size();x++)
+      {
+        for (int i = 0; i < unformattedData.get(0).length();i++)
+        {
+          if(unformattedData.get(0).substring(i,i+1).equals(","))
+          {
+            formattedData[x][t] = unformattedData.get(0).substring(base,i);
+            base = i+1;
+            t++;
+          
+          }
+        }
+      }
 
 
     } catch (FileNotFoundException e) {
@@ -48,8 +69,16 @@ public class Data {
   
   
   /* Returns the row containing the country and year. Returns -1 if not found. */
-  public int getRow(String countryName, String year) {\
-    /* finish method */
+  public int getRow(String countryName, String year) 
+  {
+    
+    for (int i = 0; i< formattedData.length; i++)
+    {
+      if (formattedData[i][0].equals(countryName)&&formattedData[i][1].equals(year))
+      {
+        return i;
+      }
+    }
 
     return -1;
   }
